bb651a0460fa7cfa03a5062422114a45dcf66af3
Jasperora <robert852654@gmail.com>
2021-12-07 12:05:41 +0800

    more modify

--- hack2/server/routes/post.js
+++ hack2/server/routes/post.js
@@ -45,7 +45,7 @@ router.post("/newPost", async (req, res) => {
     newPost.save();
     res.status(200).send({ message: "success" });
   } catch (e) {
-    res.status(403).send({ message: "error" });
+    res.status(403).send({ message: "error", post: null });
   }
 });
 // TODO 5-(1): create the 4th API (/api/post)
