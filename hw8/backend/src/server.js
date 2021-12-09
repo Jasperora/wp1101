0eea5a29c54a293ca72e3fbe5a80305054233edf
Jasperora <robert852654@gmail.com>
2021-12-09 18:51:36 +0800

    finish

--- hw8/backend/src/server.js
+++ hw8/backend/src/server.js
@@ -1,20 +1,81 @@
+import http from "http";
+import express from "express";
+import dotenv from "dotenv-defaults";
 import WebSocket from "ws";
 import mongoose from "mongoose";
+import { sendData, sendStatus, initData } from "./wssConnect";
+import Message from "./models/Message";
+
+dotenv.config();
+
+if (!process.env.MONGO_URL) {
+  console.error("Missing MONGO_URL");
+  process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  throw new Error("DB connection error: " + error);
+});
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const db = mongoose.connection;
+
+const broadcastMessage = (data, status) => {
+  wss.clients.forEach((client) => {
+    sendData(data, client);
+    sendStatus(status, client);
+  });
+};
 
 db.once("open", () => {
+  console.log("MongoDB connected!");
+
   wss.on("connection", (ws) => {
+    initData(ws);
     ws.onmessage = async (byteString) => {
-      await dbMessage.save();
+      const { data } = byteString;
+      const [task, payload] = JSON.parse(data);
+      switch (task) {
+        case "input": {
+          const { name, body } = payload;
+          const message = new Message({ name, body });
+          try {
+            await message.save();
+          } catch (e) {
+            throw new Error("Message DB save error: " + e);
+          }
+          broadcastMessage(["output", [payload]], {
+            type: "success",
+            msg: "Message sent.",
+          });
+          break;
+        }
+        case "clear": {
+          Message.deleteMany({}, () => {
+            sendData(["cleared"], ws);
+          });
+          broadcastMessage(["cleared", [payload]], {
+            type: "info",
+            msg: "Message cache cleared.",
+          });
+          break;
+        }
+
+        default:
+          break;
+      }
     };
-    //sendData(['output',[payload]])
   });
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-    console.log(`Server is up on port ${port}.`);
+  const port = process.env.PORT || 4000;
+  server.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
   });
 });
--- hw8/frontend/src/App.js
+++ hw8/frontend/src/App.js
@@ -1,26 +1,53 @@
 import "./App.css";
-import { useState } from "react";
-import { Button, Input, Tag } from "antd";
+import { useEffect, useState, useRef } from "react";
+import { Button, Input, message, Tag } from "antd";
 import useChat from "./useChat";
+import SignIn from "./Containers/signIn";
+
+const LOCALSTORAGE_KEY = "save-me";
 
 function App() {
-  const { status, messages, sendMessage } = useChat();
+  const savedMe = localStorage.getItem(LOCALSTORAGE_KEY);
+  const { status, messages, sendMessage, clearMessages } = useChat();
   const [username, setUsername] = useState("");
   const [body, setBody] = useState("");
+  const [signedIn, setSignedIn] = useState(false);
+  const [me, setMe] = useState(savedMe || "");
+  const bodyRef = useRef(null);
 
-  const client = new WebSocket("ws://localhost:4000");
-  const sendData = async (data) => {
-    await client.send(JSON.stringify(data));
+  const displayStatus = (payload) => {
+    if (payload.msg) {
+      const { type, msg } = payload;
+      const content = {
+        content: msg,
+        duration: 0.5,
+      };
+      switch (type) {
+        case "success":
+          message.success(content);
+          break;
+        case "error":
+        default:
+          message.error(content);
+          break;
+      }
+    }
   };
 
-  const sendMessage = (payload) => {
-    sendData(["input", payload]);
-  };
-  return (
+  useEffect(() => {
+    if (signedIn) {
+      localStorage.setItem(LOCALSTORAGE_KEY, me);
+    }
+  }, [signedIn, me]);
+  useEffect(() => {
+    displayStatus(status);
+  }, [status]);
+
+  return signedIn ? (
     <div className="App">
       <div className="App-title">
-        <h1>Simple Chat</h1>
-        <Button type="primary" danger>
+        <h1>{me}'s Chatroom</h1>
+        <Button type="primary" danger onClick={clearMessages}>
           Clear
         </Button>
       </div>
@@ -29,30 +56,48 @@ function App() {
           <p style={{ color: "#ccc" }}>No messages...</p>
         ) : (
           messages.map(({ name, body }, i) => {
-            <p className="App-message" key={i}>
-              <Tag color="blue">{name}</Tag>
-              {body}
-            </p>;
+            return (
+              <p className="App-message" key={i}>
+                <Tag color="blue">{name}</Tag>
+                {body}
+              </p>
+            );
           })
         )}
       </div>
       <Input
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            bodyRef.current.focus();
+          }
+        }}
         placeholder="Username"
-        value={username}
+        value={me}
         onChange={(e) => setUsername(e.target.value)}
         style={{ marginBottom: 10 }}
+        disabled
       ></Input>
       <Input.Search
+        ref={bodyRef}
         value={body}
         onChange={(e) => setBody(e.target.value)}
         enterButton="Send"
         placeholder="Type a message here..."
         onSearch={(msg) => {
-          sendMessage({ name: username, body: msg });
+          if (!msg) {
+            displayStatus({
+              type: "error",
+              msg: "Please enter a username and a message body",
+            });
+            return;
+          }
+          sendMessage({ name: me, body: msg });
           setBody("");
         }}
       ></Input.Search>
     </div>
+  ) : (
+    <SignIn me={me} setMe={setMe} setSignedIn={setSignedIn} />
   );
 }
 
