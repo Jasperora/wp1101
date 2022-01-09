869f1cfdb8b8662169bd4ce688198e007ef0d209
Jasperora <robert852654@gmail.com>
2022-01-09 08:41:09 +0800

    hack1 ref code

--- hack1/src/containers/MineSweeper.js
+++ hack1/src/containers/MineSweeper.js
@@ -17,36 +17,46 @@ const MineSweeper = () => {
   const [boardSize, setBoardSize] = useState(8); // A integer variable to store the board size in the game. The default value is 8.
 
   {
-    /* Useful Hint: The four functions below only need to fill up with those three hook functions above. */
+    /* -- TODO 1 -- */
   }
   {
-    /* -- TODO 1-2 -- */
+    /* Useful Hint: The four functions below only need to fill up with those three hook functions above. */
   }
   const startGameOnClick = () => {
     setStartGame(true);
   };
-  {
-    /* -- TODO 6-2 -- */
-  }
-  const mineNumOnChange = () => {};
-  {
-    /* -- TODO 6-2 -- */
-  }
-  const boardSizeOnChange = () => {};
-  {
-    /* -- TODO 5-2 -- */
-  }
-  const backToHomeOnClick = () => {};
+  const mineNumOnChange = (value) => {
+    setMineNum(value);
+  };
+  const boardSizeOnChange = (value) => {
+    setBoardSize(value);
+  };
+  const backToHomeOnClick = () => {
+    setStartGame(false);
+    setMineNum(10);
+    setBoardSize(8);
+  };
 
   return (
     <div className="mineSweeper">
-      {/* -- TODO 1-1 -- */}
+      {/* -- TODO 1 -- */}
       {/* Useful Hint: If ... <HomePage startGameOnClick = {startGameOnClick} .../> else <Board .../> */}
       {/* Reminder: You can refer to the structure file in p.6 of Hack1.pdf. */}
-      {!startGame ? (
-        <HomePage startGameOnClick={startGameOnClick} />
+
+      {startGame ? (
+        <Board
+          boardSize={boardSize}
+          mineNum={mineNum}
+          backToHome={backToHomeOnClick}
+        />
       ) : (
-        <Board />
+        <HomePage
+          startGameOnClick={startGameOnClick}
+          mineNumOnChange={mineNumOnChange}
+          boardSizeOnChange={boardSizeOnChange}
+          mineNum={mineNum}
+          boardSize={boardSize}
+        />
       )}
     </div>
   );
